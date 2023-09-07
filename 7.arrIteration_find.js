f3f50d796fdef2092ddf3f5fa872146c71414814
syafiq <misteredwarv321@gmail.com>
2023-09-08 02:16:26 +0800

    updating 7.find

---
+++ 7.arrIteration_find.js
@@ -0,0 +1,29 @@
+const people = [
+    {
+        name: "syafiq",
+        class: 19,
+        id: "D041211028",
+        job: "developer",
+    },
+    {
+        name: "nabil",
+        class: 14,
+        id: "D041211011",
+        job: "editor",
+    },
+    {
+        name: "syafiq",
+        class: 11,
+        id: "D041211022",
+        job: "konsumer",
+    },
+];
+
+// numberOver12 = (i) => {
+//     return i.name === 'syafiq'
+// }
+
+//NOTE: find() return an object
+const aNumOver12 = people.find((name) => name.id === "D041211011");
+
+console.log(aNumOver12);
